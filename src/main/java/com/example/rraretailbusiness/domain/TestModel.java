5e5bfc242f074a01d39782a76a17df69cd0c05e9
Ilysse200 <ilysseted500@gmail.com>
2024-01-17 19:47:48 +0200

    Added sales report in the test Model

--- src/main/java/com/example/rraretailbusiness/domain/TestModel.java
+++ src/main/java/com/example/rraretailbusiness/domain/TestModel.java
@@ -1,11 +1,36 @@
 package com.example.rraretailbusiness.domain;
 import com.example.rraretailbusiness.dao.HibernateUtil;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+import org.hibernate.Session;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 public class TestModel {
 
     public TestModel() {
     }
     public static void main(String[] args){
-        HibernateUtil.getSessionFactory().openSession();
+
+        try {
+            Session session =HibernateUtil.getSessionFactory().openSession();
+            Connection connection = session.doReturningWork(connection1 -> connection1);
+
+            JasperDesign design = JRXmlLoader.load("src/main/resources/reports/Sales.jrxml");
+            JasperReport jasperReport = JasperCompileManager.compileReport(design);
+             JasperPrint jasperPrint =JasperFillManager.fillReport(jasperReport, null, connection );
+            if (jasperPrint.getPages().isEmpty()) {
+                System.out.println("No pages in the report");
+            }
+            JasperViewer.viewReport(jasperPrint, false);
+        } catch (JRException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
     }
 }
